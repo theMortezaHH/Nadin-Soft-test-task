7392c6351810e1e9736310c33c86936221979bf1
Morteza_HH <morteza.110f110.2018@gmail.com>
2023-10-18 15:23:59 +0330

    vue-i18n added to the project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue"
+import "./style.css"
+import App from "./App.vue"
+import { createI18n } from "vue-i18n"
 
-createApp(App).mount('#app')
+const { availableLocales } = useI18n()
+
+//i18n config
+const messages = Object.fromEntries(
+    Object.entries(import.meta.glob("../locales/*.yml", { eager: true })).map(([key, value]) => {
+        return [key.slice(11, -5), value.default]
+    }),
+)
+const i18n = createI18n({
+    globalInjection: true,
+    legacy: false,
+    locale: "en",
+    messages,
+})
+
+const app = createApp(App)
+
+app.use(i18n)
+app.mount("#app")
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import VueI18n from "@intlify/unplugin-vue-i18n/vite"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        vue(),
+        VueI18n({
+            runtimeOnly: true,
+            compositionOnly: true,
+            fullInstall: true,
+            include: [path.resolve(__dirname, "locales/**")],
+        }),
+    ],
 })
